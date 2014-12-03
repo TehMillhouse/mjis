1d65818e37a3c55344004bf4a918f7c4fae2dc9a
Max Wagner <max@trollbu.de>
2014-12-03 17:58:08 +0100

    Enable integration tests
    For this to work, ../mj-test must be accessible from the root of this
    repository in the filesystem.

---
+++ src/test/scala/mjis/IntegrationTest.scala
@@ -0,0 +1,17 @@
+package mjis
+
+import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+import CompilerTestMatchers.passIntegrationTest
+
+class IntegrationTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  val TestDir = "../mj-test/run/"
+
+  "The integration tests" should "produce correct output" in {
+    val testFiles = new java.io.File(TestDir).listFiles.filter(_.getName.endsWith(".mj"))
+    for (file <- testFiles) {
+      println(s"Compiling and matching $file")
+      file.getPath should passIntegrationTest()
+    }
+  }
+}
