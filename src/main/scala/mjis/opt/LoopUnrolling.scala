e1ebf5395cd7107b3cf0a1a6523970702d44fa47
Max Wagner <max@trollbu.de>
2015-02-02 19:41:21 +0100

    loop unrolling scaffolding

---
+++ src/main/scala/mjis/opt/LoopUnrolling.scala
@@ -0,0 +1,15 @@
+package mjis.opt
+
+import firm._
+import firm.nodes._
+
+import mjis.util._
+
+import scala.collection.mutable
+
+object LoopUnrolling extends Optimization() {
+
+  override def _optimize(graph: Graph): Unit = {
+
+  }
+}
